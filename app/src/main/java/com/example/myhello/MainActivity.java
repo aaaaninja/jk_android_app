7a25ebf9342200ba99a23b57b48fa66b9f849190
syoki.matsuno <syoki.matsuno@up.uhuru.jp>
2019-11-30 22:20:32 +0900

    :tada: idから引っ張って来れるように!!
    document.getElementByIdのようなもん

--- app/src/main/java/com/example/myhello/MainActivity.java
+++ app/src/main/java/com/example/myhello/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myhello;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView myText = findViewById(R.id.myText);
+        myText.setText("こんにちはこんにちは！！!!");
     }
 }
