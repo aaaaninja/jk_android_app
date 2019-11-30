a76ea082029a9a2ead6fadd9bb07117870ce3e63
syoki.matsuno <syoki.matsuno@up.uhuru.jp>
2019-11-30 22:11:58 +0900

    :tada: log用の某をいろいろ追加
    見方がわからない……

--- app/src/main/java/com/example/myhello/MainActivity.java
+++ app/src/main/java/com/example/myhello/MainActivity.java
@@ -1,14 +1,51 @@
 package com.example.myhello;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.v("LifeCycle", "onCreate");
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        Log.v("LifeCycle", "onStart");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.v("LifeCycle", "onResume");
+    }
+
+    @Override
+    public void onPause(){
+        super.onPause();
+        Log.v("LifeCycle", "onPause");
+    }
+
+    @Override
+    public void onRestart(){
+        super.onRestart();
+        Log.v("LifeCycle", "onRestart");
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.v("LifeCycle", "onStop");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.v("LifeCycle", "onDestroy");
     }
 }
